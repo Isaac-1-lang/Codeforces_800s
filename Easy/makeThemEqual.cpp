aa5c46e50c687bd2d8f27f6a44c3cc6c029524c6
NIYOBYOSE Isaac Precieux <isaac@Isaac>
2025-09-21 17:08:37 +0100

    Some of div 2 are so hard Cz I am not ready to work on them at this on this moment

---
+++ Easy/makeThemEqual.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    vector<int> a;
+    vector<int> b;
+    int k,t,n,i;
+    while (t--)
+    {
+      cin >> n;
+      for(int i=0;i<n;i++) {
+        cin >> i;
+        a.push_back(i);
+      }
+    }    
+
+    return 0;
+}
