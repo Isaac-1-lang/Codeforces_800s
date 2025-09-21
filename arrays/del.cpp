a26080686914bab6f8e605a4a015a18602ae1df6
NIYOBYOSE Isaac Precieux <isaac@Isaac>
2025-09-21 17:50:14 +0100

    Able to delete an element at a given pos

--- arrays/del.cpp
+++ arrays/del.cpp
@@ -3,5 +3,13 @@ using namespace std;
 
 
 int main() {
-    
+    int arr[] = {21,32,24,340,12};
+    int k=2;
+    for(int i=k;i<=sizeof(arr)/sizeof(arr[0]);i++) {
+        arr[i]=arr[k+1];
+    }
+    for(int i=0;i<sizeof(arr)/sizeof(arr[0])-1;i++) {
+       cout << arr[i] << " ";
+    }
+    return 0;
 }
