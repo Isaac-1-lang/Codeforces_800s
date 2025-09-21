bfb151a9c07353f179ef47f2c009b1cb1498c426
NIYOBYOSE Isaac Precieux <isaac@Isaac>
2025-09-21 17:46:21 +0100

    Arrays have to be finished and other things needed for competitive programming

---
+++ arrays/findTheSIze.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+
+    int arr[] = {21,22,23,24,25,26,27,28,29,30,4302,1202,30202,302303,21,101,54,20,1,102,34,213,4043,};
+    int count =0;
+    for(int i:arr) {
+       count++;
+    }
+    cout << count << endl;
+    return 0;
+}
